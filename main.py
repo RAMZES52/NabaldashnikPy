65dc7aad13d96d8e158925e09e4a7c12033fba7a
katarinspz <katarinaspz743@gmail.com>
2025-02-02 03:14:21 +0300

    menu.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import pygame
 
-WIDTH = 1920
-HEIGHT = 1080
-
 running = True
-
 pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+width, height = screen.get_size()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+TOP = int(height * 0.2)
+LEFT = int(width * 0.35)
 
-# Координаты левого верхнего угла поля
-TOP = HEIGHT * 0.2
-LEFT = WIDTH * 0.35
-# Ширина и высота клетки
-CELL_H = (HEIGHT * 0.55) / 8
-CELL_W = (WIDTH * 0.30) / 8
+CELL_H = int((height * 0.55) / 8)
+CELL_W = int((width * 0.30) / 8)
 
-map = list()
-
+map = []
 
 class Cell:
     def __init__(self, x, y, color=None):
@@ -36,34 +30,41 @@ class Cell:
         else:
             return [1, self.status, color]
 
+    def draw(self, screen):
+        rect = pygame.Rect(self.x, self.y, CELL_W, CELL_H)
+
+        if self.color is not None:
+            pygame.draw.rect(screen, self.color, rect)
+
+        pygame.draw.rect(screen, "black", rect, 2)
+
 
 for i in range(8):
-    a = list()
+    columns = []
     for j in range(8):
-        a.append(Cell(i, j, None))
-    map.append(a)
+        columns.append(Cell(i, j, None))
+    map.append(columns)
 
 
-def reder():
-    # Отрисовка поля
-    pygame.draw.rect(screen, [139, 69, 19], pygame.Rect(LEFT, TOP,
-                                                        CELL_W * 8, CELL_H * 8))
-    pygame.draw.lines(screen, 'black', True,
-                      [(LEFT, TOP),
-                       (LEFT + CELL_W * 8, TOP),
-                       (LEFT + CELL_W * 8, TOP + CELL_H * 8),
-                       (LEFT, TOP + CELL_H * 8)], 3)
+def render():
+    pygame.draw.rect(screen, [139, 69, 19], pygame.Rect(LEFT, TOP, CELL_W * 8, CELL_H * 8))
+
+    pygame.draw.lines(
+        screen,
+        'black',
+        True,
+        [
+            (LEFT, TOP),
+            (LEFT + CELL_W * 8, TOP),
+            (LEFT + CELL_W * 8, TOP + CELL_H * 8),
+            (LEFT, TOP + CELL_H * 8)
+        ],
+        3
+    )
 
     for row in map:
-        for cel in row:
-            if not cel.status:
-                pygame.draw.rect(screen, cel.color, pygame.Rect(cel.x, cel.y, CELL_W, CELL_H + 1))
-
-    for line in range(8):
-        pygame.draw.line(screen, "black", (LEFT + (line * CELL_W), TOP),
-                         (LEFT + (line * CELL_W), TOP + CELL_H * 8), 2)
-        pygame.draw.line(screen, "black", (LEFT, TOP + line * CELL_H),
-                         (LEFT + CELL_W * 8, TOP + line * CELL_H), 2)
+        for cell in row:
+            cell.draw(screen)
 
 
 while running:
@@ -72,7 +73,7 @@ while running:
             running = False
 
     screen.fill([210, 105, 30])
-    reder()
+    render()
     pygame.display.flip()
 
 pygame.quit()
